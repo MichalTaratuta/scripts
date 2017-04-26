f27822f5a5af4ff9bbf8e31389fbb217395218b1
Michal Taratuta <m.taratuta@hotmail.com>
2017-04-26 16:54:40 +0100

    working HPOneView_get_CPU_Cores.py

--- hponview_list.py
+++ hponview_list.py
@@ -1,19 +1,24 @@
 
-
-from sys import argv
 import json
+import os.path
+import csv
 
-#txt = open("/tmp/oneview1", "r").readlines()
-#for x in range(4):
+servers_total=0
+processor_count_total=0
+processor_core_count_total=0
 
-	with open("/tmp/hponeview", "r") as json_data:
+for x in range(1, 5):
+	file_path = os.path.join('/home/michal/Downloads', 'hponeview_'+str(x))
+
+	with open(file_path, "r") as json_data:
 		i = json.load(json_data)
 		for x in i['members']:
-			print('Name:' + ' ' + x['name'])
-			print('Pocessor Count:' + ' ' + str(x['processorCount']))
-			print('Processor Core Count:' + ' ' + str(x['processorCoreCount']))
-			print('##########################')
+			print('Name:' + ' ' + x['name'] + ',' + ' ' + 'Pocessor Count:' + ' ' + str(x['processorCount']) + ',' + ' ' + 'Processor Core Count:' + ' ' + str(x['processorCoreCount']))
+			servers_total += 1
+			processor_count_total += x['processorCount']
+			processor_core_count_total += x['processorCoreCount']
 
 
-
-
+print('Total number of Servers:' + ' ' + str(servers_total))
+print('Processor Count total:' + ' ' + str(processor_count_total))
+print('Processor Core Count total:' + ' ' + str(processor_core_count_total))
